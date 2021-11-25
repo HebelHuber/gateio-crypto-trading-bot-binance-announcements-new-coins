1915e4d0182576a5eb94486bb87e9fd892d8b951
Lukas Kiefer <kiefermaniac@gmail.com>
2021-11-25 15:12:43 +0100

    add telegram config, clone my fork until there's a better way

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,12 @@ FROM python:3.9-alpine3.13
 
 WORKDIR /bot
 
+# RUN apk update && apk add git yq && \
+#     git clone https://github.com/CyberPunkMetalHead/gateio-crypto-trading-bot-binance-announcements-new-coins.git . && \
+#     pip3 install -r requirements.txt
+
 RUN apk update && apk add git yq && \
-    git clone https://github.com/CyberPunkMetalHead/gateio-crypto-trading-bot-binance-announcements-new-coins.git . && \
+    git clone --branch telegram-notifications https://github.com/HebelHuber/gateio-crypto-trading-bot-binance-announcements-new-coins.git . && \
     pip3 install -r requirements.txt
 
 ADD entrypoint.sh /
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,6 @@
 
 # write provided env vars into the config files
 
-# rm /bot/auth/auth.yml
 mv /bot/auth/auth.example.yml /bot/auth/auth.yml
 
 ${AUTH_GATEIO_API+"false"} || yq w -i -- /bot/auth/auth.yml 'gateio_api' "${AUTH_GATEIO_API}"
@@ -10,11 +9,6 @@ ${AUTH_GATEIO_SECRET+"false"} || yq w -i -- /bot/auth/auth.yml 'gateio_secret' "
 ${AUTH_TELEGRAM_TOKEN+"false"} || yq w -i -- /bot/auth/auth.yml 'telegram_token' "${AUTH_TELEGRAM_TOKEN}"
 ${AUTH_TELEGRAM_CHATID+"false"} || yq w -i -- /bot/auth/auth.yml 'telegram_chat_id' "${AUTH_TELEGRAM_CHATID}"
 
-echo "========= Content of auth.yml"
-cat /bot/auth/auth.yml
-echo "========= END"
-
-# rm /bot/config.yml
 mv /bot/config.example.yml /bot/config.yml
 
 ${TRADE_OPTIONS_QUANTITY+"false"} || yq w -i -- /bot/config.yml 'TRADE_OPTIONS.QUANTITY' "${TRADE_OPTIONS_QUANTITY}"
@@ -28,19 +22,18 @@ ${TRADE_OPTIONS_TTP+"false"} || yq w -i -- /bot/config.yml 'TRADE_OPTIONS.TTP' "
 ${LOGGING_LOG_LEVEL+"false"} || yq w -i -- /bot/config.yml 'LOGGING.LOG_LEVEL' "${LOGGING_LOG_LEVEL}"
 ${LOGGING_LOG_FILE+"false"} || yq w -i -- /bot/config.yml 'LOGGING.LOG_FILE' "${LOGGING_LOG_FILE}"
 ${LOGGING_LOG_TO_CONSOLE+"false"} || yq w -i -- /bot/config.yml 'LOGGING.LOG_TO_CONSOLE' "${LOGGING_LOG_TO_CONSOLE}"
-# ${TELEGRAM_ENABLED+"false"} || yq w -i -- /bot/config.yml  "${TELEGRAM_ENABLED}"
-# ${TELEGRAM_NOTIFICATIONS_STARTUP+"false"} || yq w -i -- /bot/config.yml  "${TELEGRAM_NOTIFICATIONS_STARTUP}"
-# ${TELEGRAM_NOTIFICATIONS_COIN_ANNOUNCEMENT+"false"} || yq w -i -- /bot/config.yml  "${TELEGRAM_NOTIFICATIONS_COIN_ANNOUNCEMENT}"
-# ${TELEGRAM_NOTIFICATIONS_COIN_NOT_SUPPORTED+"false"} || yq w -i -- /bot/config.yml  "${TELEGRAM_NOTIFICATIONS_COIN_NOT_SUPPORTED}"
-# ${TELEGRAM_NOTIFICATIONS_BUY_START+"false"} || yq w -i -- /bot/config.yml  "${TELEGRAM_NOTIFICATIONS_BUY_START}"
-# ${TELEGRAM_NOTIFICATIONS_BUY_ORDER_CREATED+"false"} || yq w -i -- /bot/config.yml  "${TELEGRAM_NOTIFICATIONS_BUY_ORDER_CREATED}"
-# ${TELEGRAM_NOTIFICATIONS_BUY_FILLED+"false"} || yq w -i -- /bot/config.yml  "${TELEGRAM_NOTIFICATIONS_BUY_FILLED}"
-# ${TELEGRAM_NOTIFICATIONS_SELL_START+"false"} || yq w -i -- /bot/config.yml  "${TELEGRAM_NOTIFICATIONS_SELL_START}"
-# ${TELEGRAM_NOTIFICATIONS_SELL_FILLED+"false"} || yq w -i -- /bot/config.yml  "${TELEGRAM_NOTIFICATIONS_SELL_FILLED}"
+${TELEGRAM_ENABLED+"false"} || yq w -i -- /bot/config.yml 'TELEGRAM.ENABLED' "${TELEGRAM_ENABLED}"
+${TELEGRAM_NOTIFICATIONS_STARTUP+"false"} || yq w -i -- /bot/config.yml 'TELEGRAM.NOTIFICATIONS.STARTUP' "${TELEGRAM_NOTIFICATIONS_STARTUP}"
+${TELEGRAM_NOTIFICATIONS_COIN_ANNOUNCEMENT+"false"} || yq w -i -- /bot/config.yml 'TELEGRAM.NOTIFICATIONS.COIN_ANNOUNCEMENT' "${TELEGRAM_NOTIFICATIONS_COIN_ANNOUNCEMENT}"
+${TELEGRAM_NOTIFICATIONS_COIN_NOT_SUPPORTED+"false"} || yq w -i -- /bot/config.yml 'TELEGRAM.NOTIFICATIONS.COIN_NOT_SUPPORTED' "${TELEGRAM_NOTIFICATIONS_COIN_NOT_SUPPORTED}"
+${TELEGRAM_NOTIFICATIONS_BUY_START+"false"} || yq w -i -- /bot/config.yml 'TELEGRAM.NOTIFICATIONS.BUY_START' "${TELEGRAM_NOTIFICATIONS_BUY_START}"
+${TELEGRAM_NOTIFICATIONS_BUY_ORDER_CREATED+"false"} || yq w -i -- /bot/config.yml 'TELEGRAM.NOTIFICATIONS.BUY_ORDER_CREATED' "${TELEGRAM_NOTIFICATIONS_BUY_ORDER_CREATED}"
+${TELEGRAM_NOTIFICATIONS_BUY_FILLED+"false"} || yq w -i -- /bot/config.yml 'TELEGRAM.NOTIFICATIONS.BUY_FILLED' "${TELEGRAM_NOTIFICATIONS_BUY_FILLED}"
+${TELEGRAM_NOTIFICATIONS_SELL_START+"false"} || yq w -i -- /bot/config.yml 'TELEGRAM.NOTIFICATIONS.SELL_START' "${TELEGRAM_NOTIFICATIONS_SELL_START}"
+${TELEGRAM_NOTIFICATIONS_SELL_FILLED+"false"} || yq w -i -- /bot/config.yml 'TELEGRAM.NOTIFICATIONS.SELL_FILLED' "${TELEGRAM_NOTIFICATIONS_SELL_FILLED}"
 
 echo "========= Content of config.yml"
 cat /bot/config.yml
 echo "========= END"
 
-# python /bot/main.py
-# sleep 120
+python /bot/main.py
